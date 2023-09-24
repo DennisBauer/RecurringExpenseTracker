752b3bc8f6494938269952c91fbbd4d7f95c09cd
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2023-09-24 18:25:44 +0200

    Adjust versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
