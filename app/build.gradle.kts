3e3c2b4b44063c1b5c6ef9cd1ae6dabc158b3897
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2023-10-07 19:53:10 +0200

    Create new release 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.dbauer.expensetracker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
