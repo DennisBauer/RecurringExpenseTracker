c959ca983f0df1b334145827adab3c456b504b3e
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2023-10-29 20:06:41 +0100

    Add backup and restore option
    fixes #25

--- app/src/main/java/de/dbauer/expensetracker/ui/SettingsScreen.kt
+++ app/src/main/java/de/dbauer/expensetracker/ui/SettingsScreen.kt
@@ -1,7 +1,6 @@
 package de.dbauer.expensetracker.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,37 +11,63 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import de.dbauer.expensetracker.R
 import de.dbauer.expensetracker.ui.theme.ExpenseTrackerTheme
 
 @Composable
-fun SettingsScreen(modifier: Modifier = Modifier) {
+fun SettingsScreen(
+    onBackupClicked: () -> Unit,
+    onRestoreClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
         modifier =
             modifier
                 .verticalScroll(rememberScrollState())
                 .fillMaxSize(),
     ) {
-        Text(
-            text = "More to come soon",
-            modifier = Modifier.padding(16.dp),
+        SettingsHeaderElement(
+            header = R.string.settings_backup,
+        )
+        SettingsClickableElement(
+            name = R.string.settings_backup_create,
+            onClick = onBackupClicked,
+        )
+        SettingsClickableElement(
+            name = R.string.settings_backup_restore,
+            onClick = onRestoreClicked,
         )
     }
 }
 
 @Composable
+private fun SettingsHeaderElement(
+    @StringRes header: Int,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = stringResource(id = header),
+        style = MaterialTheme.typography.bodyMedium,
+        color = MaterialTheme.colorScheme.primary,
+        modifier =
+            modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Composable
 private fun SettingsClickableElement(
     @StringRes name: Int,
-    onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    onClick: () -> Unit,
 ) {
     Surface(
         color = Color.Transparent,
@@ -67,7 +92,10 @@ private fun SettingsClickableElement(
 private fun SettingsScreenPreview() {
     ExpenseTrackerTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
-            SettingsScreen()
+            SettingsScreen(
+                onBackupClicked = {},
+                onRestoreClicked = {},
+            )
         }
     }
 }
