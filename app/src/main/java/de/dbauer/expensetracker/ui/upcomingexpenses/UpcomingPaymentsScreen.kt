897e799003ff8df0be7752e9736ee24bdfea28ae
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2024-02-02 17:09:04 +0100

    Fix minor warning for upcoming payments icon

--- app/src/main/java/de/dbauer/expensetracker/ui/upcomingexpenses/UpcomingPaymentsScreen.kt
+++ app/src/main/java/de/dbauer/expensetracker/ui/upcomingexpenses/UpcomingPaymentsScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.automirrored.rounded.List
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -155,7 +155,7 @@ fun UpcomingPaymentsOverviewPlaceholder(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
     ) {
         Icon(
-            imageVector = Icons.Rounded.List,
+            imageVector = Icons.AutoMirrored.Rounded.List,
             contentDescription = null,
             modifier = Modifier.size(100.dp),
         )
