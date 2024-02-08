34e553df9b84f9149c7149c2eec332f1e9bce155
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2024-02-08 19:42:19 +0100

    Adjust to new Compose changes

--- app/src/main/java/de/dbauer/expensetracker/ui/editexpense/FirstPaymentOption.kt
+++ app/src/main/java/de/dbauer/expensetracker/ui/editexpense/FirstPaymentOption.kt
@@ -11,8 +11,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Cancel
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -77,13 +77,13 @@ fun FirstPaymentOption(
             if (date != INVALID_DATE) {
                 IconButton(onClick = {
                     onDateSelected(INVALID_DATE)
-                    datePickerState.setSelection(System.currentTimeMillis())
+                    datePickerState.selectedDateMillis = System.currentTimeMillis()
                 }) {
                     Icon(imageVector = Icons.Rounded.Cancel, contentDescription = null)
                 }
             }
         }
-        Divider(
+        HorizontalDivider(
             thickness = 1.dp,
             color = MaterialTheme.colorScheme.onSurfaceVariant,
         )
