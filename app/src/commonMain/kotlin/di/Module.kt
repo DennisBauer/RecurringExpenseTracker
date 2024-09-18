2536d55339a3020e817b0f7457f9ba8291597849
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2024-09-18 20:06:14 +0200

    Fix deprecation by migrating to new Koin API

--- app/src/commonMain/kotlin/di/Module.kt
+++ app/src/commonMain/kotlin/di/Module.kt
@@ -5,10 +5,10 @@ import model.ExchangeRateProvider
 import model.database.ExpenseRepository
 import model.database.RecurringExpenseDao
 import model.database.RecurringExpenseDatabase
-import org.koin.compose.viewmodel.dsl.viewModel
-import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import viewmodel.EditRecurringExpenseViewModel
