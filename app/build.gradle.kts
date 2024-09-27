cc5c8bc12b2c2831aeecbaf580651796dce91dcc
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2024-09-27 21:31:28 +0200

    Create new release 0.12.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,8 @@ android {
         applicationId = "de.dbauer.expensetracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 26
-        versionName = "0.12.2"
+        versionCode = 27
+        versionName = "0.12.3"
     }
     signingConfigs {
         create("release") {
