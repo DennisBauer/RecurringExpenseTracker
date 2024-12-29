96357b4e87fd5af409c25aaf03950e63ff344e6d
Dominik Dias <dias_dominik@icloud.com>
2024-12-29 01:20:35 +0100

    Add Glance dependency, export common Res strings
    The common Res strings are added as sourceSets and can be imported but
    when using them in non common Code it gives a compile error that it
    can not find them. Exposing them as public solves this.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ kotlin {
 
             implementation(libs.koin.android)
             implementation(libs.koin.androidx.compose)
+            implementation(libs.androidx.glance.appwidget)
+            implementation(libs.androidx.glance.material3)
         }
         commonMain.dependencies {
             implementation(compose.components.resources)
@@ -154,14 +156,19 @@ android {
     }
 }
 
-compose.desktop {
-    application {
-        mainClass = "MainKt"
+compose {
+    resources {
+        publicResClass = true
+    }
+    desktop {
+        application {
+            mainClass = "MainKt"
 
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "de.dbauer.expensetracker"
-            packageVersion = "1.0.0"
+            nativeDistributions {
+                targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+                packageName = "de.dbauer.expensetracker"
+                packageVersion = "1.0.0"
+            }
         }
     }
 }
