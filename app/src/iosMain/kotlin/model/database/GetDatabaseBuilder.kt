232a1234b371abb83129dbefa1b0e7a48c2d7833
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2025-04-13 11:09:50 +0200

    Migrate to stable Room 2.7.0 release

--- app/src/iosMain/kotlin/model/database/GetDatabaseBuilder.kt
+++ app/src/iosMain/kotlin/model/database/GetDatabaseBuilder.kt
@@ -3,11 +3,27 @@ package model.database
 import Constants
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<RecurringExpenseDatabase> {
-    val dbFile = "${NSHomeDirectory()}/${Constants.DATABASE_NAME}"
+    val dbFilePath = "${documentDirectory()}/${Constants.DATABASE_NAME}"
     return Room.databaseBuilder<RecurringExpenseDatabase>(
-        name = dbFile,
+        name = dbFilePath,
     )
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory =
+        NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null,
+        )
+    return requireNotNull(documentDirectory?.path)
 }
