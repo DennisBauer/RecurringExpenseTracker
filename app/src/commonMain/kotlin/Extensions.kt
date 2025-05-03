c731152d1ffe3362fca182a493a7279b52727904
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2025-05-03 18:03:31 +0200

    Adjust expense color based on user theme

--- app/src/commonMain/kotlin/Extensions.kt
+++ app/src/commonMain/kotlin/Extensions.kt
@@ -1,12 +1,18 @@
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import model.DateTimeCalculator
+import model.database.UserPreferencesRepository
 import model.getSystemCurrencyCode
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.getString
+import org.koin.compose.koinInject
+import ui.ThemeMode
 
 expect fun Float.toCurrencyString(currencyCode: String): String
 
@@ -39,4 +45,15 @@ fun LocalDate.getNextPaymentDays(): Int {
 
 suspend fun Flow<String>.getDefaultCurrencyCode(): String {
     return first().ifBlank { getSystemCurrencyCode() }
+}
+
+@Composable
+fun useDarkTheme(): Boolean {
+    val userPreferencesRepository = koinInject<UserPreferencesRepository>()
+    val selectedTheme by userPreferencesRepository.themeMode.collectAsState()
+    return when (selectedTheme) {
+        ThemeMode.Dark.value -> true
+        ThemeMode.Light.value -> false
+        else -> isSystemInDarkTheme()
+    }
 }
