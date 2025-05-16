104136cd6f1e8c9e2668481779f40aaaa1da1724
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2025-05-16 22:11:31 +0200

    Create new release 0.17.2
    * Fix crash on app startup with last version if notifications were enabled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         applicationId = "de.dbauer.expensetracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 40
-        versionName = "0.17.1"
+        versionCode = 41
+        versionName = "0.17.2"
     }
     signingConfigs {
         create("release") {
