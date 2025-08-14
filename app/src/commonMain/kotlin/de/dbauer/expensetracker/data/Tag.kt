557e1ea1c9503d423ff30d04e64c0588342928ec
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2025-08-14 19:16:54 +0200

    Properly insert, update and delete expense tags

--- app/src/commonMain/kotlin/de/dbauer/expensetracker/data/Tag.kt
+++ app/src/commonMain/kotlin/de/dbauer/expensetracker/data/Tag.kt
@@ -1,7 +1,3 @@
 package de.dbauer.expensetracker.data
 
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
-
-@OptIn(ExperimentalUuidApi::class)
-data class Tag(val title: String, val color: Long, val id: Int = Uuid.random().hashCode())
+data class Tag(val title: String, val color: Long, val id: Int = 0)
