c0147e5af339013ebd4dae737c35444f2ef10165
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2025-08-24 11:50:16 +0200

    Create new release 0.17.8
    This release fixes an F-Droid build issue: #656
    * Update exchange rates
    * Updated (Spanish) translation
    * Added (Portuguese) translation
    * Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,8 +113,8 @@ android {
         applicationId = "de.dbauer.expensetracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 46
-        versionName = "0.17.7"
+        versionCode = 47
+        versionName = "0.17.8"
     }
     signingConfigs {
         create("release") {
