9957548300f8610cffea11ff055ffbde1155be78
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2025-10-20 21:32:05 +0200

    Create new release 0.19.1
    * Fix crash adding a new expense with reminders enabled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,8 +117,8 @@ android {
         applicationId = "de.dbauer.expensetracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 52
-        versionName = "0.19.0"
+        versionCode = 53
+        versionName = "0.19.1"
     }
     signingConfigs {
         create("release") {
