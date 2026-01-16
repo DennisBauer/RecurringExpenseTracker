cde7226e092142a29d99e6303b00dc81e1d5e6c2
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2026-01-16 17:57:02 +0100

    Remove hot reload plugin as it is already bundled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.composeHotReload)
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
     alias(libs.plugins.kotlin.serialization)
