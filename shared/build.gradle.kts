8630229c5dc4074793ead56d4755f04806c6bd08
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2026-01-22 19:18:38 +0100

    Define minSdkVersion to prevent legacy permissions added to the manifest
    Defining a minSdkVersion for the shared lib prevents some legacy
    permissions to be added to the AndroidManifest.xml:
    android.permission.READ_PHONE_STATE
    android.permission.READ_EXTERNAL_STORAGE
    android.permission.WRITE_EXTERNAL_STORAGE
    
    fixes: #827

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
     androidLibrary {
         namespace = "de.dbauer.expensetracker.shared"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
 
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_21)
