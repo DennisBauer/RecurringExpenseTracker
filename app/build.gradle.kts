53d01e3e421422f0a8009874e4515850b2daaf83
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2026-05-18 18:55:47 +0200

    Create new release 0.21.1
    * Fix: Upcoming expenses show up further into the future than the end date
    * Translations updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "de.dbauer.expensetracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 64
-        versionName = "0.21.0"
+        versionCode = 65
+        versionName = "0.21.1"
 
         buildFeatures {
             buildConfig = true
